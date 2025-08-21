e1cf0d987b83b01111ab08b8b82e2ee3d30f98f0
larikhide <petrakliy@gmail.com>
2025-08-21 21:43:10 +0300

    add hello world

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello, world!")
+print("What is your name?")
+my_name = input('>')
+print("Is is good to meet you " + my_name)
+print("The lenght of your name is:", len(my_name))
+print("What is your age?")
+my_age = input('>')
+print("You will be " + str(int(my_age)+1) + " in a year")
