c5f3d8622ba8a6f082c12cbe0b1595c79b47894f
larikhide <petrakliy@gmail.com>
2025-09-18 20:44:20 +0300

    add flow control examples

---
+++ flowControl/littleKid.py
@@ -0,0 +1,9 @@
+name = 'Carol'
+age = 3000
+
+if name == 'Alice':
+    print('Hello, Alice')
+elif age < 12:
+    print('You are not Alice, kiddo')
+else:
+    print('You are either not Alice nor little kid')
---
+++ flowControl/oppositeday.py
@@ -0,0 +1,13 @@
+today_is_opposite_day = True
+
+if today_is_opposite_day ==True:
+    say_it_opposite_way = True
+else:
+    say_it_opposite_way = False
+
+if today_is_opposite_day == True:
+            say_it_opposite_way = not say_it_opposite_way
+if say_it_opposite_way == True:
+        print('Today is an opposite day')
+else:
+        print('Today is not an opposite day')
